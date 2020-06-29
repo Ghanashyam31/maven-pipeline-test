40fbbfeed0ae86e276fb3e08556a1476db02a7ef
Ghanashyam31 <ghanashyam.patole17@gmail.com>
2020-06-29 06:02:08 -0400

    updated the mvn files ........

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,7 +3,8 @@
 echo "************************************************************"
 echo "    --------  BUILDING THE JAR FILE  -------------          "
 echo "************************************************************"
-WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/final-pipeline'
+WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/maven-pipeline-test'
+#/home/jenkin/jenkins-data/maven-pipeline-test/
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -3,8 +3,7 @@
 echo "************************************************************"
 echo "    --------  TESTING THE JAR FILE  -------------          "
 echo "************************************************************"
+WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/maven-pipeline-test'
 
-WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/final-pipeline'
-
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
