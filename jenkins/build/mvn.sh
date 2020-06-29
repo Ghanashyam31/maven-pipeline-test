52ce32d69e24802bbc9579e33e64f5c561e9f218
Ghanashyam31 <ghanashyam.patole17@gmail.com>
2020-06-29 10:17:59 -0400

    Updated the WORKSPACE in mvn.sh files

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,7 +3,7 @@
 echo "************************************************************"
 echo "    --------  BUILDING THE JAR FILE  -------------          "
 echo "************************************************************"
-WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/maven-pipeline-test'
+WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/mav-project1-pipeline-29june2020'
 #/home/jenkin/jenkins-data/maven-pipeline-test/
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -3,7 +3,7 @@
 echo "************************************************************"
 echo "    --------  TESTING THE JAR FILE  -------------          "
 echo "************************************************************"
-WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/maven-pipeline-test'
+WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/mav-project1-pipeline-29june2020'
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
