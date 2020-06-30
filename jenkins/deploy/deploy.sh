e704941bad3c49d1c09c1cdb09681a629f25c1ed
Ghanashyam31 <ghanashyam.patole17@gmail.com>
2020-06-30 07:34:23 -0400

    replaced hostname with ipaddress in deploy.sh script

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -8,7 +8,7 @@ echo "$IMAGE" > /tmp/.auth
 echo "$BUILD_ID" >> /tmp/.auth
 echo "$PASS" >> /tmp/.auth
 
-scp /tmp/.auth produser@jenkin-prod:/tmp/.auth
-scp jenkins/deploy/publish.sh produser@jenkin-prod:/tmp/publish.sh
+scp /tmp/.auth produser@192.168.1.57:/tmp/.auth
+scp jenkins/deploy/publish.sh produser@192.168.1.57:/tmp/publish.sh
 
-ssh produser@jenkin-prod "/tmp/publish.sh"
+ssh produser@192.168.1.57 "/tmp/publish.sh"
