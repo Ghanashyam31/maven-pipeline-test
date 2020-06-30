a803bd6352e54c80090a96a942b1f1a43d3de504
Ghanashyam31 <ghanashyam.patole17@gmail.com>
2020-06-30 05:56:37 -0400

    Updated deploy.sh script

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+export IMAGE=maven-project
+export BUILD_ID=10
+export PASS=Power@#1234
 
 echo "$IMAGE" > /tmp/.auth
 echo "$BUILD_ID" >> /tmp/.auth
 echo "$PASS" >> /tmp/.auth
 
 scp /tmp/.auth produser@jenkin-prod:/tmp/.auth
-scp jenkin/deploy/publish.sh produser@jenkin-prod:/tmp/publish.sh
+scp jenkins/deploy/publish.sh produser@jenkin-prod:/tmp/publish.sh
 
 ssh produser@jenkin-prod "/tmp/publish.sh"
