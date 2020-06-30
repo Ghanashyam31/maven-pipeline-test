7b8e62291e714c740882b2313a36dc97014fb46b
Ghanashyam31 <ghanashyam.patole17@gmail.com>
2020-06-30 07:47:31 -0400

    added key in deploy.sh script

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -8,7 +8,7 @@ echo "$IMAGE" > /tmp/.auth
 echo "$BUILD_ID" >> /tmp/.auth
 echo "$PASS" >> /tmp/.auth
 
-scp /tmp/.auth produser@192.168.1.57:/tmp/.auth
-scp jenkins/deploy/publish.sh produser@192.168.1.57:/tmp/publish.sh
+scp -i /tmp/id_rsa /tmp/.auth produser@192.168.1.57:/tmp/.auth
+scp -i /tmp/id_rsa jenkins/deploy/publish.sh produser@192.168.1.57:/tmp/publish.sh
 
-ssh produser@192.168.1.57 "/tmp/publish.sh"
+ssh -i /tmp/id_rsa produser@192.168.1.57 "/tmp/publish.sh"
