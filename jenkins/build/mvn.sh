2335c8d719c00152aea65d1a29f58eca941a57f8
Ghanashyam31 <ghanashyam.patole17@gmail.com>
2020-07-02 03:37:27 -0400

    corrected build files

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,14 +3,8 @@
 echo "************************************************************"
 echo "    --------  BUILDING THE JAR FILE  -------------          "
 echo "************************************************************"
-<<<<<<< HEAD
 WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/mav-project1-pipeline-29june2020'
 #/home/jenkin/jenkins-data/maven-pipeline-test/
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
-=======
-WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/final-pipeline'
-
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
->>>>>>> 1b5f2db4bf2353a86d2a93c0a4dcbc21ed594d15
 
