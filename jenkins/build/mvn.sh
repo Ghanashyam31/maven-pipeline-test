5e0a570a14f1847d4af6034d5ec00a94ceac364e
Ghanashyam31 <ghanashyam.patole17@gmail.com>
2020-07-02 03:41:32 -0400

    mvn.sh build file changed

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -7,4 +7,3 @@ WORKSPACE='/home/jenkin/jenkins-data/jenkins_home/workspace/mav-project1-pipelin
 #/home/jenkin/jenkins-data/maven-pipeline-test/
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
-
